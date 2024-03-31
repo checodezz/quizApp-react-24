a8f32b38eed31e5fe92baaabe067b963a0e8680e
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-03-31 17:54:05 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const questions = [
+    {
+      id: 1,
+      question: "What is 2 + 2?",
+      options: ["3", "4", "5"],
+      correctAnswer: "4",
+    },
+    {
+      id: 2,
+      question: "What is the capital of France?",
+      options: ["London", "Paris", "Berlin"],
+      correctAnswer: "Paris",
+    },
+    {
+      id: 3,
+      question: 'Who wrote "To Kill a Mockingbird"?',
+      options: ["Harper Lee", "J.K. Rowling", "Stephen King"],
+      correctAnswer: "Harper Lee",
+    },
+  ];
+
+  const currentQuestionObj = questions[currentQuestionIndex];
+
+  const handleAnswerChange = (event) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+  };
+
+  return (
+    <main>
+      <h1>Quiz App</h1>
+      <h2>Question {currentQuestionObj.id}</h2>
+      <p>{currentQuestionObj.question}</p>
+      <ul>
+        {currentQuestionObj.options.map((option, index) => (
+          <li key={index}>
+            <label>
+              <input
+                type="radio"
+                value={option}
+                checked={selectedOption === option}
+                name={`quest_${currentQuestionObj.id}`}
+                onChange={handleAnswerChange}
+              />
+              {option}
+            </label>
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => setCurrentQuestionIndex((prevValue) => prevValue + 1)}
+      >
+        Next
+      </button>
+    </main>
+  );
+}
